4db9629ee0973dd9350a0c97e6fe8ef8ec6b260a
Rudy Eko Prasetya <rudyekoprasetya@yahoo.co.id>
2024-11-08 13:08:32 +0700

    [ADD] func untuk hapus file lama sebelum upload file baru

--- app.py
+++ app.py
@@ -9,10 +9,25 @@ upload_folder = os.path.join('static','uploads')
 
 app.config['UPLOAD'] = upload_folder
 
+# fungsi hapus gambar di folder uploads
+def delete_files_in_directory(directory_path):
+   try:
+     with os.scandir(directory_path) as entries:
+       for entry in entries:
+         if entry.is_file():
+            os.unlink(entry.path)
+     print("All files deleted successfully.")
+   except OSError:
+     print("Error occurred while deleting files.")
+
+
 # index
 @app.route("/", methods=['GET','POST'])
 def home():
 	if request.method == 'POST':
+		# hapus file lama
+		delete_files_in_directory(app.config['UPLOAD'])
+		# upload file baru
 		file = request.files['img']
 		filename = secure_filename(file.filename)
 		file.save(os.path.join(app.config['UPLOAD'], filename))
